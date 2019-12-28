d0857143b91fb88b06ac1c900e62f88f3f2bb5ff
Gaute Berge <gauteab@MacBook-Pro>
2019-12-28 22:34:09 +0100

    rendered concept

--- src/Main.elm
+++ src/Main.elm
@@ -15,12 +15,12 @@ import List.Extra as List
 
 
 type alias Model =
-    Expression
+    ConceptNode
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( example, Cmd.none )
+    ( example2, Cmd.none )
 
 
 
@@ -46,7 +46,7 @@ view model =
         [ Font.family [ Font.monospace ]
         , paddingXY 5 5
         ]
-        (renderExpression [ "case", "list", "int" ] model)
+        (renderConcept model)
 
 
 
@@ -66,6 +66,10 @@ main =
 example : Expression
 example =
     Case (String "abc") [ ( Character 'a', List [ Integer 1, Integer 2, Case (String "xyz") [ ( Character 'b', List [ Integer 2, Character 'c' ] ) ] ] ), ( Integer 1, Integer 2 ) ]
+
+
+example2 =
+    case_ 0 (string 1 "abc") [ ( string 2 "a", list 7 [ int 3 1, int 4 2, case_ 5 (string 6 "xyz") [ ( string 8 "b", list 9 [ int 10 2 ] ) ] ] ), ( int 11 1, int 12 2 ) ]
 
 
 elementInfo : String -> Expression -> { name : String, multiline : Bool }
@@ -93,6 +97,84 @@ type alias Query =
     List String
 
 
+type alias Id =
+    Int
+
+
+runQuery : List Id -> Query -> ConceptNode -> List Id
+runQuery found query conceptNode =
+    let
+        ( queryHead, queryTail ) =
+            List.uncons query |> Maybe.withDefault ( "", [] )
+
+        ( newQuery, newFound ) =
+            case ( queryMatch queryHead conceptNode.name, List.isEmpty queryTail ) of
+                ( True, True ) ->
+                    ( [], conceptNode.id :: found )
+
+                ( True, False ) ->
+                    ( queryTail, found )
+
+                _ ->
+                    ( query, found )
+    in
+    Debug.todo ""
+
+
+codeElement2 =
+    let
+        ( multiline, highlight ) =
+            ( True, False )
+
+        style =
+            [ Background.color (rgb255 0 0 255) ]
+    in
+    case ( multiline, highlight ) of
+        ( False, False ) ->
+            row []
+
+        ( False, True ) ->
+            row style
+
+        ( True, False ) ->
+            column []
+
+        ( True, True ) ->
+            column style
+
+
+renderConcept : ConceptNode -> Element msg
+renderConcept conceptNode =
+    case conceptNode.concept of
+        IntC x ->
+            codeElement2 [ text (String.fromInt x) ]
+
+        StringC s ->
+            codeElement2 [ text <| "\"" ++ s ++ "\"" ]
+
+        CaseC e es ->
+            let
+                renderBranch ( pattern, expr ) =
+                    column []
+                        [ row [] [ renderConcept pattern, text " -> " ]
+                        , el [ paddingXY 25 0 ] (renderConcept expr)
+                        ]
+            in
+            codeElement2
+                [ row [] [ text "case", el [ paddingXY 5 0 ] <| renderConcept e, text "of" ]
+                , column [ paddingXY 25 0 ] <| List.map renderBranch es
+                ]
+
+        ListC [] ->
+            codeElement2 []
+
+        ListC (x :: xs) ->
+            codeElement2 <|
+                row [] [ text "[", renderConcept x ]
+                    :: List.map (\e -> row [] [ el [ alignTop ] (text ","), renderConcept e ]) xs
+                    ++ [ text "]" ]
+
+
 renderExpression : Query -> Expression -> Element msg
 renderExpression query expression =
     let
@@ -116,7 +198,7 @@ renderExpression query expression =
         indent =
             paddingXY 25 0
 
-        codeElement e =
+        codeElement_ e =
             let
                 style =
                     if highlight then
@@ -143,7 +225,7 @@ renderExpression query expression =
                         , el [ indent ] (renderExpression newQuery expr)
                         ]
             in
-            codeElement expression
+            codeElement_ expression
                 [ row [] [ text "case", el [ paddingXY 5 0 ] <| renderExpression newQuery e, text "of" ]
                 , column [ indent ] <| List.map renderBranch patterns
                 ]
@@ -151,8 +233,8 @@ renderExpression query expression =
         String s ->
             text <| "\"" ++ s ++ "\""
 
-        Integer int ->
-            codeElement expression <| [ text (String.fromInt int) ]
+        Integer x ->
+            codeElement_ expression <| [ text (String.fromInt x) ]
 
         Float float ->
             text <| String.fromFloat float
@@ -161,7 +243,7 @@ renderExpression query expression =
             text "[]"
 
         List (x :: xs) ->
-            codeElement expression <|
+            codeElement_ expression <|
                 row [] [ text "[", renderExpression newQuery x ]
                     :: List.map (\e -> row [] [ el [ alignTop ] (text ","), renderExpression newQuery e ]) xs
                     ++ [ text "]" ]
@@ -183,6 +265,20 @@ renderExpression query expression =
 -}
 
 
+type alias ConceptNode =
+    { name : String
+    , id : Id
+    , concept : Concept
+    }
+
+
+type Concept
+    = IntC Int
+    | StringC String
+    | CaseC ConceptNode (List ( ConceptNode, ConceptNode ))
+    | ListC (List ConceptNode)
+
+
 type Expression
     = Character Char
     | String String
@@ -201,3 +297,27 @@ type Expression
     | Lambda (List Expression) Expression
     | Application Expression Expression
     | BinOp Expression Expression Expression
+
+
+defaultInfo =
+    { name = "", selected = False }
+
+
+case_ : Int -> ConceptNode -> List ( ConceptNode, ConceptNode ) -> ConceptNode
+case_ id e branches =
+    { name = "case", id = id, concept = CaseC e branches }
+
+
+string : Int -> String -> ConceptNode
+string id e =
+    { name = "string", id = id, concept = StringC e }
+
+
+int : Int -> Int -> ConceptNode
+int id e =
+    { name = "int", id = id, concept = IntC e }
+
+
+list : Int -> List ConceptNode -> ConceptNode
+list id e =
+    { name = "list", id = id, concept = ListC e }
